43df4f2465dcd51e500047bc8858782b35d37870
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-08-25 11:29:59 +0300

    Add project files.

---
+++ EF-CORE/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        var configration = new ConfigurationBuilder()
+            .AddJsonFile("appstiing.json")
+            .Build();
+
+        SqlConnection connction = new SqlConnection(configration.GetSection("constr").Value);
+        var sql = "SELECT * FROM WALLETS";
+
+        SqlCommand cmd = new SqlCommand(sql, connction);
+
+        cmd.CommandType = CommandType.Text;
+
+        connction.Open();
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        Wallet wallet;
+
+        while (reader.Read()) {
+
+            wallet = new Wallet
+            {
+
+                Id = reader.GetInt32("Id"),
+                Holder = reader.GetString("Holder"),
+                Balance = reader.GetDecimal("Balance")
+            };
+
+            Console.WriteLine(wallet);
+
+
+        }
+        connction.Close();
+    }
+}
